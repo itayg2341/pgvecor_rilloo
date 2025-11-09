ba9493dac503c95b5a07f062158eb8a7c4ee8946
test <test@example.com>
2025-11-09 14:55:59 +0000

    A concise summary of the fix

--- src_cpp/bitutils.h
+++ src_cpp/bitutils.h
@@ -8,9 +8,17 @@
 #error "Requires PostgreSQL 13+"
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern uint64 (*BitHammingDistance) (uint32 bytes, unsigned char *ax, unsigned char *bx, uint64 distance);
 extern double (*BitJaccardDistance) (uint32 bytes, unsigned char *ax, unsigned char *bx, uint64 ab, uint64 aa, uint64 bb);
 
-void		BitvecInit(void);
+voidBitvecInit(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
